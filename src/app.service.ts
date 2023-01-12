a2760510d54cc9382142890c5729a0f33c136e26
Rodrigo Quintas <rodrigo.quintas@savecash.com.br>
2023-01-12 14:49:58 -0300

    feat: implemented serverless file and test endpoints

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHelloGet(): string {
+    return 'Hello World GET!';
+  }
+
+  getHelloPost(): string {
+    return 'Hello World POST!';
   }
 }
